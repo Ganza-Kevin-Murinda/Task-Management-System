8cec073c9e32859c3c2661a563974e3af7b55822
kevin <murindakevinmulk@gmail.com>
2025-05-18 23:49:38 +0200

    Update Category Service.

--- src/main/java/com/tsm/task/service/interfaces/CategoryService.java
+++ src/main/java/com/tsm/task/service/interfaces/CategoryService.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 public interface CategoryService {
     boolean createCategory(Category category);
+
     Category getCategoryById(int id) throws SQLException;
+
     List<Category> getAllCategories();
+
     boolean updateCategory(Category category);
+
     boolean deleteCategory(int id);
 }
